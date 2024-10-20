c7836a9a5eb077ebd646dcfae24de42afd7f1284
Wwhds_one <a1605691832@163.com>
2024-10-20 11:05:51 +0800

    调整格式化信息使用结构体存储，同时调整新建参数

---
+++ config/format.go
@@ -0,0 +1,17 @@
+package config
+
+import "go.uber.org/zap/zapcore"
+
+// LogFormatConfig 用于指定日志的各项格式化设置
+type LogFormatConfig struct {
+	// 日志级别
+	Level zapcore.Level
+	// 日志前缀
+	Prefix string
+	// 是否采用json格式
+	IsJson bool
+	// 编码格式
+	EncoderLevel string
+	// 打印堆栈信息的等级
+	StacktraceLevel zapcore.LevelEnabler
+}
--- core/zapLog.go
+++ core/zapLog.go
@@ -1,11 +1,11 @@
-package zapLog
+package core
 
 import (
-	"io"
-	"os"
-
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"io"
+	"os"
+	"zapLog/config"
 )
 
 type Level = zapcore.Level
@@ -24,21 +24,32 @@ type Logger struct {
 	al *zap.AtomicLevel
 }
 
-func New(out io.Writer, level Level) *Logger {
-	if out == nil {
-		out = os.Stderr
+func Build(out []io.Writer, l config.LogFormatConfig) *Logger {
+
+	// 初始化日志等级，有对应的动态调整接口
+	al := zap.NewAtomicLevelAt(l.Level)
+
+	// 初始化日志编码格式
+	encoder := Encoder(l.Prefix, l.EncoderLevel, l.IsJson)
+
+	// 创建多个输出目标
+	var writers = make([]zapcore.WriteSyncer, 0, len(out))
+
+	// 默认输出到控制台
+	writers = append(writers, zapcore.AddSync(os.Stdout))
+
+	// 添加自定义输出目标
+	for _, w := range out {
+		writers = append(writers, zapcore.AddSync(w))
 	}
 
-	al := zap.NewAtomicLevelAt(level)
-	cfg := zap.NewProductionEncoderConfig()
-	cfg.EncodeTime = zapcore.RFC3339TimeEncoder
+	// 创建日志输出对象
+	core := zapcore.NewCore(encoder, zapcore.NewMultiWriteSyncer(writers...), al)
 
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(cfg),
-		zapcore.AddSync(out),
-		al,
-	)
-	return &Logger{l: zap.New(core), al: &al}
+	return &Logger{
+		l:  zap.New(core, zap.AddStacktrace(l.StacktraceLevel)),
+		al: &al,
+	}
 }
 
 func (l *Logger) SetLevel(level Level) {
@@ -77,12 +88,12 @@ func (l *Logger) Sync() error {
 	return l.l.Sync()
 }
 
-var std = New(os.Stderr, InfoLevel)
+var std = Build([]io.Writer{}, InfoLevel, "[Zap]", false)
 
 func Default() *Logger         { return std }
 func ReplaceDefault(l *Logger) { std = l }
 
-func SetLevel(level Level) { std.SetLevel(level) }
+func SetLevel() { std.SetLevel(level) }
 
 func Debug(msg string, fields ...Field) { std.Debug(msg, fields...) }
 func Info(msg string, fields ...Field)  { std.Info(msg, fields...) }
