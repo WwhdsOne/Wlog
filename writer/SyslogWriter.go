9dc297f9c01c8409546c112749fa9e6f978c9982
wangzhongning <zhongning.wang@zhipuai.cn>
2024-10-30 17:42:01 +0800

    write raw data to udp instead of useing syslog package

--- writer/SyslogWriter.go
+++ writer/SyslogWriter.go
@@ -1,30 +1,57 @@
 package writer
 
 import (
+	"fmt"
 	"log"
 	"log/syslog"
-	"strconv"
+	"net"
 )
 
+func init() {
+	socket, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   net.IPv4(10, 50, 34, 72),
+		Port: 51898,
+	})
+	if err != nil {
+		fmt.Println("连接服务端失败，err:", err)
+		return
+	}
+	defer socket.Close()
+	fmt.Println(string(b))
+	x, err := socket.Write(b)
+	if err != nil {
+		fmt.Println("发送数据失败，err:", err)
+		return
+	}
+	fmt.Println("发送成功，发送字节数：", x)
+}
+
 type SyslogWriter struct {
-	Network   string          //网络通信方式
-	Host      string          // ip地址
-	Port      int             // 端口
-	Priority  syslog.Priority // 优先级
-	Tag       string          // 标签
-	SysWriter *syslog.Writer  // syslog writer
+	Network  string          //网络通信方式
+	Host     string          // ip地址
+	Port     int             // 端口
+	Priority syslog.Priority // 优先级
+	Tag      string          // 标签
+	conn     *net.UDPConn
 }
 
 // InitWriter 初始化SysWriter
 func (s *SyslogWriter) InitWriter() {
-	writer, err := syslog.Dial(s.Network, s.Host+":"+strconv.Itoa(s.Port), s.Priority, s.Tag)
+	conn, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   net.ParseIP(s.Host),
+		Port: s.Port,
+	})
 	if err != nil {
 		log.Printf("Failed to connect to syslog server: %s\n", err)
+
 	}
-	s.SysWriter = writer
+	s.conn = conn
 }
 
-// 提供io.Writer
 func (s *SyslogWriter) Write(p []byte) (n int, err error) {
-	return s.SysWriter.Write(p)
+	return s.conn.Write(p)
+}
+
+func (s *SyslogWriter) Close() error {
+	return s.conn.Close()
 }
