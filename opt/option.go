da2b49a0f8d176994056e5de56c0bc1811562d27
wangzhongning <zhongning.wang@zhipuai.cn>
2024-11-21 01:19:08 +0800

    add ctx

--- opt/option.go
+++ opt/option.go
@@ -1,6 +1,10 @@
 package opt
 
+import "context"
+
 type Option interface {
 	//FormatMessage(msgID string, lv int, msg string) string
 	FormatMessage(msgID string, lv int, format string, args []any) string
+	WithContext(ctx context.Context)
+	WithContextKeys(keys []any)
 }
